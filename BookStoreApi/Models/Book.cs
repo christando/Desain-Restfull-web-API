fcef5b6a91f7720a8f210fa1c75226d8a59fa85d
Valentino Raviendra Christando <93866810+christando@users.noreply.github.com>
2023-04-22 00:38:20 +0700

    Pertemuan 7 Drwa

--- BookStoreApi/Models/Book.cs
+++ BookStoreApi/Models/Book.cs
@@ -1,11 +1,13 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
 
 namespace BookStoreApi.Models;
 
 public class Book
 {
     [BsonId]
+    [JsonPropertyName("Name")]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -1,14 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
 using BookStoreApi.Models;
 using BookStoreApi.Services;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
 
-builder.Services.AddSingleton<BooksService>();
+    builder.Services.AddSingleton<BooksService>();
 
 builder.Services.AddControllers();
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
